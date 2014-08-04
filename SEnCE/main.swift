874c489ae54116b1da19ef4fe2c28359614872f7
Andreas Kostuch <andreas.kostuch@tlr-newmedia.com>
2014-08-04 12:11:24 +0200

    Initial Commit

---
+++ SEnCE/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SEnCE
+//
+//  Created by Andreas Kostuch on 04.08.14.
+//  Copyright (c) 2014 Andreas Kostuch. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ SEnCETests/SEnCETests.swift
@@ -0,0 +1,36 @@
+//
+//  SEnCETests.swift
+//  SEnCETests
+//
+//  Created by Andreas Kostuch on 04.08.14.
+//  Copyright (c) 2014 Andreas Kostuch. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SEnCETests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
